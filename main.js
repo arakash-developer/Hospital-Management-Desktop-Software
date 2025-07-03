6cbbe2ab75b360264287a951ba7d23bb55361e2c
akash100a <arakash968786@gmail.com>
2025-07-03 23:16:04 +0600

    Add yauzl package version 2.10.0 for unzip functionality in Node.js

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Hospital Management Desktop Software",
+    // icon: __dirname + '/assets/icon.png',
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Create the start URL
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file:",
+    slashes: true,
+  });
+
+  // Load the HTML file using the start URL
+  mainWindow.loadURL(startUrl);
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
